1944cf4eb81b6bff7b0d8a1e2e7129cde051e11b
AlbertOy <albert2302oy@gmail.com>
2023-06-16 23:51:57 +0700

    third commit

--- app/src/main/java/huyoy/project/makoil02/MainActivity.java
+++ app/src/main/java/huyoy/project/makoil02/MainActivity.java
@@ -1,14 +1,36 @@
 package huyoy.project.makoil02;
 
+
+
+import static androidx.navigation.ui.BottomNavigationViewKt.setupWithNavController;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import huyoy.project.makoil02.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        navMain();
+    }
+
+    private void navMain() {
+        NavHostFragment navHostFragment= (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController= navHostFragment.getNavController();
+        BottomNavigationView bottomNavigationView= findViewById(R.id.bottomNavigationView);
+        setupWithNavController(bottomNavigationView,navController);
+
     }
 }
