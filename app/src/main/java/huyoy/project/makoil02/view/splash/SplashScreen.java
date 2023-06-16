2310ce814e80c4f712dc113776ef1e030828f73f
AlbertOy <albert2302oy@gmail.com>
2023-06-16 21:54:25 +0700

    fist commit

---
+++ app/src/main/java/huyoy/project/makoil02/view/splash/SplashScreen.java
@@ -0,0 +1,16 @@
+package huyoy.project.makoil02.view.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import huyoy.project.makoil02.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
