93122e9e3d9d647d2c2a34c565eb3d081ad6d44f
AlbertOy <albert2302oy@gmail.com>
2023-06-16 23:10:57 +0700

    second commit

--- app/src/main/java/huyoy/project/makoil02/view/splash/SplashScreen.java
+++ app/src/main/java/huyoy/project/makoil02/view/splash/SplashScreen.java
@@ -2,15 +2,33 @@ package huyoy.project.makoil02.view.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
+import huyoy.project.makoil02.MainActivity;
 import huyoy.project.makoil02.R;
+import huyoy.project.makoil02.databinding.ActivitySplashScreenBinding;
 
 public class SplashScreen extends AppCompatActivity {
+    private ActivitySplashScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        binding=ActivitySplashScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        nextAct();
+    }
+
+    private void nextAct() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i= new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finishAffinity();
+            }
+        },2000);
     }
 }
